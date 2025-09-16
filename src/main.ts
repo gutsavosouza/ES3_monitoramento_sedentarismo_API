42e12210d8406dfcc43257c7ecaf0d3efd5cf2cf
gutsavosouza <gustavosouza1688@gmail.com>
2025-09-16 12:40:30 -0300

    refactor: adds setup for validation pipe and cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const PORT = process.env.PORT ?? 8080;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || '*',
+  });
+
+  await app.listen(PORT);
 }
 bootstrap();

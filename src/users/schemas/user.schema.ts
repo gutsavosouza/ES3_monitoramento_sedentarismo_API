4eb01f8c0da6cb9c9347981b83342b02df002740
gutsavosouza <gustavosouza1688@gmail.com>
2025-09-18 15:22:13 -0300

    refactor & fix: makes it it so the user class extends Document, fixed the wrong Document class being retrieved in this code, also adds optional fields to user, following documentation

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,10 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { UserRole } from '../enums/user-role.enum';
+import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 
 @Schema({ timestamps: true })
-export class User {
+export class User extends Document {
   @Prop({ required: true, unique: true })
   email: string;
 
@@ -16,6 +17,15 @@ export class User {
 
   @Prop({ required: true, type: Number, enum: UserRole })
   role: UserRole;
+
+  @Prop({ required: false, default: null })
+  school: string;
+
+  @Prop({ required: false, default: null })
+  birthdate: Date;
+
+  @Prop({ required: false, default: null })
+  city: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);

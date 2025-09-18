5baa9a6fbcc44943d7ea2bba506811abdc68aef5
gutsavosouza <gustavosouza1688@gmail.com>
2025-09-18 15:23:01 -0300

    feat: adds new endpoint to user controller, to handle request to register new students

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post('register-student')
+  async registerStudent(@Body() createUserDTO: CreateUserDTO) {
+    return this.userService.createStudent(createUserDTO);
+  }
+}

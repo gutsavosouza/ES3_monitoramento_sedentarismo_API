9ca7a4a615628c9700c03e3581a11e8cdeb1d963
gutsavosouza <gustavosouza1688@gmail.com>
2025-09-18 15:23:54 -0300

    refactor: adds studentdata repository to providers list and also importing student data schema to be used in repository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,12 +4,17 @@ import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { StudentDataRepository } from './student-data.repository';
+import { StudentData, StudentDataSchema } from './schemas/student-data.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: StudentData.name, schema: StudentDataSchema },
+    ]),
   ],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository],
+  providers: [UsersService, UsersRepository, StudentDataRepository],
 })
 export class UsersModule {}

8b3c15c5e6ee1e1170c7400308278749bd70cee8
gutsavosouza <gustavosouza1688@gmail.com>
2025-09-19 10:31:22 -0300

    refactor: updates the main app to use configure and uss swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const PORT = process.env.PORT ?? 8080;
+
+  const config = new DocumentBuilder()
+    .setTitle('ES3 - Monitoramento Sedentarismo API')
+    .setDescription(
+      'API construída para o projeto da disciplina de Engenharia de Software 3, com base nos requisitos levantdos pelo cliente e pela equipe.',
+    )
+    .setVersion('0.1.0')
+    .addServer(`http://localhost:${PORT}`)
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
 
   app.useGlobalPipes(
     new ValidationPipe({

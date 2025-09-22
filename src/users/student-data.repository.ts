f6650c0eaa8d7f2072bf36b1b8bf79bb6102914b
gutsavosouza <gustavosouza1688@gmail.com>
2025-09-22 11:35:27 -0300

    feat: adds new functions to repository, to handle update and retrieval of student data

--- src/users/student-data.repository.ts
+++ src/users/student-data.repository.ts
@@ -4,7 +4,8 @@ import {
   StudentData,
   StudentDataDocument,
 } from './schemas/student-data.schema';
-import mongoose, { Model } from 'mongoose';
+import { Model } from 'mongoose';
+import { UpdateStudentInfoDTO } from './dtos/update-student-info.dto';
 
 @Injectable()
 export class StudentDataRepository {
@@ -17,5 +18,18 @@ export class StudentDataRepository {
     const createdStudentData = await this.studentDataModel.create({ userId });
 
     return createdStudentData;
+  }
+
+  async findById(userId: any): Promise<StudentData | null> {
+    return this.studentDataModel.findOne({ userId: userId }).exec();
+  }
+
+  async updateStudentData(
+    userId: any,
+    updateData: UpdateStudentInfoDTO,
+  ): Promise<StudentData | null> {
+    return this.studentDataModel
+      .findOneAndUpdate({ userId: userId }, { $set: updateData }, { new: true })
+      .exec();
   }
 }

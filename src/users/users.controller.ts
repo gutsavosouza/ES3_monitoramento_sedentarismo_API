611df83595766fa14a2eddb887bff7adf3ad1c2c
gutsavosouza <gustavosouza1688@gmail.com>
2025-09-22 11:39:59 -0300

    feat: adds requests to update student and general user data and retrieve student data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { ApiBody, ApiResponse } from '@nestjs/swagger';
+import { UpdateUserPersonalInfoDTO } from './dtos/update-user-personal-info.dto';
+import { UpdateStudentInfoDTO } from './dtos/update-student-info.dto';
 
 @Controller('users')
 export class UsersController {
@@ -18,8 +20,54 @@ export class UsersController {
       'Conflict error, theres already an user with the received email',
   })
   @ApiResponse({ status: 500, description: 'Internal server error' })
-  @Post('register-student')
+  @Post('students')
   async registerStudent(@Body() createUserDTO: CreateUserDTO) {
     return this.userService.createStudent(createUserDTO);
+  }
+
+  @ApiBody({ type: UpdateUserPersonalInfoDTO })
+  @ApiResponse({
+    status: 200,
+    description: 'User personal info updated successfully.',
+  })
+  @ApiResponse({ status: 404, description: 'User not found.' })
+  @Patch('personal-info/:email')
+  async updateUserPersonalInfo(
+    @Param('email') email: string,
+    @Body() updateData: UpdateUserPersonalInfoDTO,
+  ) {
+    return this.userService.updateUserPersonalData(email, updateData);
+  }
+
+  @ApiResponse({
+    status: 200,
+    description: 'Student data retrieved successfully.',
+  })
+  @ApiResponse({ status: 404, description: 'User not found.' })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden: User is not a student.',
+  })
+  @Get('students/data/:email')
+  async getStudentData(@Param('email') email: string) {
+    return this.userService.getStudentInfo(email);
+  }
+
+  @ApiBody({ type: UpdateStudentInfoDTO })
+  @ApiResponse({
+    status: 200,
+    description: 'Student info updated successfully.',
+  })
+  @ApiResponse({ status: 404, description: 'User not found.' })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden: User is not a student.',
+  })
+  @Patch('students/data/:email')
+  async updateStudentInfo(
+    @Param('email') email: string,
+    @Body() updateData: UpdateStudentInfoDTO,
+  ) {
+    return this.userService.updateStudentData(email, updateData);
   }
 }
